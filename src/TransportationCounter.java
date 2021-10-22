b8ddc243e06908c525cb4da0abef932c3a11d619
Charles Couture <90707535+CharlesCouturee@users.noreply.github.com>
2021-10-22 14:33:40 -0400

    Update TransportationCounter.java

--- src/TransportationCounter.java
+++ src/TransportationCounter.java
@@ -12,6 +12,7 @@ public class TransportationCounter {
     private CounterType type;
     private String imageFilepath;
     private ImageIcon image;
+    private JLabel display;
     // add JLabel
 
     public TransportationCounter (CounterType pType)
@@ -22,13 +23,15 @@ public class TransportationCounter {
         // since the images are named similarly and ordered the same way as they are in the enum declaration, we can get the filepath just by using the type.
 
         int imageNumber = this.type.ordinal() + 1; // since the images start from M01, not M00
-        this.imageFilepath = ("M0" + imageNumber + ".png");
+        this.imageFilepath = ("/assets/sprites/M0" + imageNumber + ".png");
         this.image = new ImageIcon (this.imageFilepath);
         Image toResize = this.image.getImage();
         Image resized = toResize.getScaledInstance(90, 130,  java.awt.Image.SCALE_SMOOTH);
         this.image = new ImageIcon(resized);
 
         // add JLabel
+        
+        this.display = new JLabel(this.image);
     }
 
     private ImageIcon getImage() {return this.image;}
@@ -37,7 +40,7 @@ public class TransportationCounter {
 
     public String getImageFilepath() {return this.imageFilepath;}
 
-
+    public JLabel getDisplay() {return this.display;}
 
 
 
