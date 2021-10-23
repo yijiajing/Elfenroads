ab93c68f1a888c3585a5a7001fd91909a7afd507
RexBergy <67751720+RexBergy@users.noreply.github.com>
2021-10-23 13:33:44 -0400

    Added panel variables

--- src/StartWindow.java
+++ src/StartWindow.java
@@ -6,13 +6,32 @@ public class StartWindow extends JPanel{
 
     private JFrame startFrame;
     private int width;
-    private int length;
+    private int height;
+
+    private JPanel mainPanel;
+    private JPanel menuPanel;
+    private JPanel buttonsPanel;
+    private JPanel playPanel;
+    private JPanel aboutPanel;
+    private JPanel exitPanel;
+
     private JButton startButton;
     private JButton aboutButton;
     private JButton exitButton;
 
     StartWindow(){
-        
+        startFrame = new JFrame("StartScreen");
+        width = startFrame.getWidth();
+        height = startFrame.getHeight();
+
+        startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    private void StartScreen(){
+    
+        mainPanel = new JPanel();
+
     }
 
 
