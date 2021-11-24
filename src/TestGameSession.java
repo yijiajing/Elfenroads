fc11e5fdfc5e72af262285dd92d14c54e43220da
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2021-11-24 15:12:15 -0500

    created TestGameSession class to test API calls

---
+++ src/TestGameSession.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class TestGameSession {
+
+
+    public static void main (String [] args) throws IOException
+    {
+        String username = "nick2";
+        String password = "Nick12345";
+
+        GameSession game = new GameSession(username, password);
+    }
+
+
+
+
+
+
+
+
+}
