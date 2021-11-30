79a9aa36f6bfb101970d245e82e7fbf4f58a268f
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2021-11-29 20:43:04 -0500

    code now creates a game session using the API

--- src/GameSession.java
+++ src/GameSession.java
@@ -7,48 +7,66 @@ import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.net.InetAddress;
 
 public class GameSession {
 
     private User creator;
-    private User gameServiceUser;
-    private JSONObject info;
-    private ArrayList<User> players;
-    private boolean launched;
-    private ArrayList<String> playerLocations;
-    private String sessionName;
+    private String gameName; // matches up to a GameService object, but we will not use a GameService object because we just need the name
     private String saveGameName;
+    private String locationIP; // this should be the system IP address
+
+    // this class will represent a GameSession
+    // its constructor will intialize a GameSession
 
 
-    public GameSession(User pCreator, String pSessionName, String pSaveName) throws IOException
-    {
-        // this constructor will use the provided User to create a new game session
-        // the User must have the appropriate (admin) permissions
-        creator = pCreator;
-        sessionName = pSessionName;
-        saveGameName = pSaveName;
-        createGameSession(creator, sessionName, saveGameName);
-    }
-
-    // if no save game name is provided to the constructor, we save the game under its session name
-    public GameSession (User pCreator, String pSessionName) throws IOException
+    public GameSession(User pCreator, String pGameName, String pSaveGameName) throws IOException
     {
         creator = pCreator;
-        sessionName = pSessionName;
-        saveGameName = pSessionName;
-        createGameSession(creator, sessionName, saveGameName);
+        gameName = pGameName;
+        saveGameName = pSaveGameName;
+        InetAddress ip = InetAddress.getLocalHost();
+        locationIP = ip.getHostAddress();
+        createNewSession();
     }
 
-    private void createGameSession(User admin, String pSessionName, String pSaveGameName) throws IOException
+
+    private void createNewSession() throws IOException
     {
-        // create a new game session with the specifications
+        String token = creator.getAccessToken();
+
+
+        URL url = new URL("http://127.0.0.1:4242/api/sessions?access_token=" + token + "&location=" + locationIP);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+
+        /* Payload support */
+        con.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(con.getOutputStream());
+        out.writeBytes("{\n");
+        out.writeBytes("    \"creator\": \"" + creator.getUsername() + "\",\n");
+        out.writeBytes("    \"game\": \"" + gameName + "\",\n");
+        out.writeBytes("    \"savegame\": \"\"\n");
+        out.writeBytes("}");
+        out.flush();
+        out.close();
+
+        int status = con.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        con.disconnect();
+        System.out.println("Response status: " + status);
+        System.out.println(content.toString());
+
+
 
     }
-
-
-
-
-
 
 
 
--- src/TestUser.java
+++ src/TestUser.java
@@ -5,8 +5,8 @@ public class TestUser {
 
     public static void main (String [] args) throws IOException
     {
-        String username = "test";
-        String password = "Password1234";
+        String username = "maex";
+        String password = "abc123_ABC123";
         String sessionName = "Nick's game";
 
         User newUser = new User(username, password); // authenticates test user
@@ -14,6 +14,7 @@ public class TestUser {
 
         // System.out.println(User.doesUserExist("nick2", newUser));
         GameService dumbGame = new GameService(newUser, "ElfenlandsGame", "Elfenlands", "Cheese12345", 5, 10);
+        GameSession session = new GameSession(newUser, "ElfenlandsGame" , "savegame12");
     }
 
 
