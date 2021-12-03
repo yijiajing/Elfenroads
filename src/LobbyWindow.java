9efbdaa7e34af0add94c6eea28e5b84c92dbcc49
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2021-12-03 10:42:25 -0500

    updated LobbyWindow

--- src/LobbyWindow.java
+++ src/LobbyWindow.java
@@ -6,7 +6,7 @@ import java.awt.*;
 import java.io.IOException;
 
 public class LobbyWindow extends JPanel implements ActionListener {
-    
+
     //private static Box boxPanel;
 
     // private String creator;
@@ -28,8 +28,10 @@ public class LobbyWindow extends JPanel implements ActionListener {
     private JLabel numPlayers;
     private Box gameInfo;
 
+    private String filepathToRepo = "/Users/charlescouture/eclipse-workspace/COMP361/src";
+
     LobbyWindow(){
-        
+
         createButton = new JButton("CREATE NEW SESSION");
         loadButton = new JButton("LOAD SAVED SESSION");
         gamesButton = new JButton("JOIN");
@@ -39,8 +41,8 @@ public class LobbyWindow extends JPanel implements ActionListener {
         // add an action listener
 
 
-        ImageIcon background_image = 
-        new ImageIcon("./assets/sprites/elfenroads.jpeg");
+        ImageIcon background_image =
+                new ImageIcon(filepathToRepo + "/assets/sprites/elfenroads.jpeg");
         background_elvenroads = new JLabel(background_image);
         gameToJoin = new JLabel();
         gameToJoin.setText("");
@@ -63,30 +65,83 @@ public class LobbyWindow extends JPanel implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 remove(background_elvenroads);
-                MainFrameCopy.mainPanel.add(new VersionToPlayWindow(), "version");
-                MainFrameCopy.cardLayout.show(MainFrameCopy.mainPanel,"version");
-                
+                MainFrame.mainPanel.add(new VersionToPlayWindow(), "version");
+                MainFrame.cardLayout.show(MainFrame.mainPanel,"version");
+
             }
-            
+
         });
         loadButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 remove(background_elvenroads);
-                MainFrameCopy.mainPanel.add(new LoadGameWindow(), "load");
-                MainFrameCopy.cardLayout.show(MainFrameCopy.mainPanel,"load");
+                MainFrame.mainPanel.add(new LoadGameWindow(), "load");
+                MainFrame.cardLayout.show(MainFrame.mainPanel,"load");
             }
         });
-        gamesButton.addActionListener(this);
+        //gamesButton.addActionListener(this);
+        gamesButton.addActionListener(new ActionListener()
+        {
 
-        refreshButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e)
+            {
+
+            }
+        });
+
+        refreshButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                gameInfo.removeAll();
+                gameInfo.repaint();
+                gameInfo.revalidate();
+                JSONObject j = null;
+                try
+                {
+                    j = GameSession.getSessions();
+                }
+                catch (IOException e1)
+                {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+
+                String id = GameSession.getFirstSessionID(j);
+                JSONObject json = null;
+
+                try
+                {
+                    json = GameSession.getGameParameters(id);
+                }
+                catch (IOException e1)
+                {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                JSONObject json2 = null;
+                try
+                {
+                    json2 = GameSession.getSessionDetails(id);
+                }
+                catch (IOException e1)
+                {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                gameInfo.add(new JLabel("creator: " + json2.get("creator")));
+                gameInfo.add(new JLabel("MaxSessionPlayers: " + json.get("maxSessionPlayers")));
+                gameInfo.add(new JLabel("MinSessionPlayers: " + json.getDouble("minSessionPlayers")));
+                gameInfo.add(new JLabel("name: " + json.get("name")));
+                gameInfo.add(gamesButton);
+                gameInfo.repaint();
+                gameInfo.revalidate();
                 // TODO: get info about game
                 /* JSONObject allSessions = GameSession.getSessions();
                 String firstSessionID = GameSession.getFirstSessionID(allSessions);
                 JSONObject sessionDetails = GameSession.getSessionDetails(firstSessionID);
-
                  */
 
             }
@@ -115,7 +170,7 @@ public class LobbyWindow extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        
+
+
     }
 }
