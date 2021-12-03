4a7b480c4065ecc6c08e902a9e5ea3da7fd9db35
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2021-12-02 19:23:53 -0500

    began work on GameState and Player classes

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public class Player {
+
+    // this represents a player in a game
+    // there may be some overlap between this and the User class, but I thought it would be a good idea to keep them separated for now
+
+    private String location; // will represent the player's IP
+
+    public Player()
+    {
+
+    }
+
+}
