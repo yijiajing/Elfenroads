672a1945aafb20a2460c50394341f16b8596ccdf
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2022-01-18 13:00:17 -0500

    adding test to TestAPI

--- src/test/TestAPI.java
+++ src/test/TestAPI.java
@@ -1,5 +1,6 @@
 package test;
 
+import networking.GameService;
 import networking.User;
 
 import java.io.IOException;
@@ -9,7 +10,8 @@ public class TestAPI {
     public static void main (String [] args) throws IOException, Exception
 
     {
-
+        User admin = new User("maex", "abc123_ABC123");
+        GameService elfenlands = new GameService()
     }
 
     public static void testCreateUser(String username, String password) throws IOException, Exception {
