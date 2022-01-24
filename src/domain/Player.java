c3a61220e9d0823226874f9367bb9ebd4c013e7a
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2022-01-24 17:36:03 -0500

    removed unused code from Player

--- src/domain/Player.java
+++ src/domain/Player.java
@@ -16,12 +16,6 @@ public class Player {
     // this represents a player in a game
     // there may be some overlap between this and the networking.User class, but I thought it would be a good idea to keep them separated for now
 
-    private InetAddress location; // will be used to connect a player
-    private String locationAsString; // will represent the player's IP
-    private ArrayList<Player> connectedPlayers;
-    private ArrayList<Socket> in; // connections in
-    private ArrayList<Socket> out; // connections out
-
     // game-specific info
     private Town curTown;
     private Set<Town> townsVisited = new HashSet<>();
@@ -34,13 +28,6 @@ public class Player {
     {
         this.curTown = GameMap.getInstance(pScreen).getTownByName("Elvenhold");
         this.colour = pColour;
-    }
-
-
-    // TODO: implement this once I finish up with P2Pconnection
-    public void addConnection(P2PConnection conWithPlayer)
-    {
-
     }
 
     public String getCurrentTownName() {
