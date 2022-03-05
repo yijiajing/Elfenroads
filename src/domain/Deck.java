a6bd32887cabf641db738b3ce01c146b008b0eb7
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2022-03-05 14:36:18 -0500

    WARNING: DOESN'T COMPILE
    committing for Yijia to look at and try to fix the Stack type definition

--- src/domain/Deck.java
+++ src/domain/Deck.java
@@ -11,14 +11,14 @@ public abstract class Deck {
 
     long seed; // the seed for any random operations. will be the game session ID
     Random rand; // the actual random object, created with the seed, that will be used to perform shuffle operations
-    Stack<Drawable> components; // the actual deck
+    Stack<E extends Drawable> components; // the actual deck
 
     protected Deck (String sessionID, ArrayList<Drawable> pComponents) // the sessionID will be turned into an integer and used as a seed
     {
         sessionID = sessionID.trim(); // just in case there were some lingering whitespaces or something
         seed = Long.parseLong(sessionID);
         rand = new Random(seed);
-        components = new Stack<Drawable>();
+        components = new Stack<E extends Drawable>();
 
         for (Drawable toAdd : pComponents)
         {
@@ -34,7 +34,7 @@ public abstract class Deck {
         sessionID = sessionID.trim();
         seed = Long.parseLong(sessionID);
         rand = new Random(seed);
-        components = new Stack <Drawable>();
+        components = new Stack <E extends Drawable>();
     }
 
     public void addDrawable(Drawable toAdd) {this.components.add(toAdd);}
