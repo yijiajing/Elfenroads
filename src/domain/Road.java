01b4bb2188c30e8154360a12b166ddb0275e040f
Chloe Pappas <chloeoliviapappas@gmail.com>
2022-03-09 12:32:02 -0500

    Remove counters from hand after they are placed on the map

--- src/domain/Road.java
+++ src/domain/Road.java
@@ -38,13 +38,14 @@ public class Road {
         if (transportationCounter.getRequiredNumOfUnitsOn(this) >= 1){
             transportationCounter.setPlacedOn(this);
             this.transportationCounter = transportationCounter;
-            counterPanel.setTransportationCounter(transportationCounter); // update UI
+            counterPanel.setTransportationCounter(transportationCounter); // update map
+            GameManager.getInstance().getThisPlayer().getHand().removeUnit(this.transportationCounter);
+            this.transportationCounter.setOwned(false);
+            GameScreen.getInstance().addTransportationCountersAndObstacle(); // update counters in hand on UI
             return true;
         } else {
             return false;
-
         }
-        
     }
 
     public TransportationCounter getTransportationCounter() {
@@ -57,7 +58,9 @@ public class Road {
         }
         this.obstacle = obstacle;
         obstacle.setPlacedOn(this);
-        counterPanel.placeObstacle(obstacle); // update UI
+        counterPanel.placeObstacle(obstacle); // update map
+        GameManager.getInstance().getThisPlayer().getHand().removeUnit(this.obstacle);
+        GameScreen.getInstance().addTransportationCountersAndObstacle(); // update obstacle in hand on UI
         return true;
     }
 
