769454dc5d590a08df7f6438dfc5761a13a805b4
Nick Triantos <nicholas.triantos@cs.mcgill.ca>
2022-03-23 16:05:28 -0400

    make sure that LobbyWindow and PlayerWaitWindow are fully deleted and reinitialized after the player leaves a game.

--- src/loginwindow/PlayerWaitWindow.java
+++ src/loginwindow/PlayerWaitWindow.java
@@ -140,8 +140,13 @@ public class PlayerWaitWindow extends JPanel implements Runnable
 
                 // go back to the lobby
                 // first, we need to destroy the old LobbyWindow. then we will add a new one.
-                LobbyWindow toDelete = MainFrame.getLobby();
-                MainFrame.mainPanel.remove(toDelete);
+                if (MainFrame.getLobby() != null)
+                {
+                    // don't try to delete a null object
+                    // this field is pretty much guaranteed to be not null, but we might as well check for safety
+                    LobbyWindow toDelete = MainFrame.getLobby();
+                    MainFrame.mainPanel.remove(toDelete);
+                }
                 // initialize and show a new LobbyWindow
                 LobbyWindow reinitialized = new LobbyWindow();
                 MainFrame.setLobbyWindow(reinitialized);
