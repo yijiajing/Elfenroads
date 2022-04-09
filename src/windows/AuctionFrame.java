84e32901cc86e3e73a3f9ebe59eec9a223b4a1ba
Huasen Xiong <huasenxiong@gmail.com>
2022-04-09 17:59:00 -0400

    Update CounterPanel

--- src/windows/AuctionFrame.java
+++ src/windows/AuctionFrame.java
@@ -427,13 +427,23 @@ public class AuctionFrame extends javax.swing.JFrame {
 
     public CounterUnit removeFirstCounter() {
         assert listCounters.size() > 0;
+        jPanel1.removeAll();
         CounterUnit counter = listCounters.remove(0);
+<<<<<<< Updated upstream
         jPanel1 = new javax.swing.JPanel();
         
         jScrollPane1.setViewportView(jPanel1);
         
         this.repaint();
         jPanel1.repaint();
+=======
+//        jPanel1 = new javax.swing.JPanel();
+//
+//        jScrollPane1.setViewportView(jPanel1);
+//
+//        this.repaint();
+//        jPanel1.repaint();
+>>>>>>> Stashed changes
         for (CounterUnit cu : listCounters){
             addCounter(cu);
         }
